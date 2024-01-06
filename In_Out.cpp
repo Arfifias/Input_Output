b91b0c4980aa5df17f53fa252b9bc692891bbb92
Aristeidis Stagkos <115788364+Arfifias@users.noreply.github.com>
2024-01-06 17:32:19 +0200

    Add files via upload

---
+++ In_Out.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main(){
+
+using namespace std;
+
+string name;
+int age;
+
+cout << "Please enter your name" << "\n";
+getline(cin >> ws,name);
+cout << "Please enter you age" << "\n";
+cin >> age;
+
+cout << "Your name is " << name << "\n";
+
+cout << "Your age is " << age;
+
+return 0;
+
+//Input and Output in C++
+//For more tutorials make sure to follow!!!  
+
+}
